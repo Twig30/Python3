412ecbf34a0db992e54035c17d1d386d2dfc1d9c
Twig30 <immanuel.delgado@stu.vertexacademies.org>
2025-05-28 18:16:20 +0000

    made additional changes

--- 321_lists_Delgado.py
+++ 321_lists_Delgado.py
@@ -8,8 +8,8 @@ print(food[3], "is located at index 3.")
 print(food[-1], "is located at index -1.")
 if("sundae" in food):
     print("You can have a sundae!")
-else print("we dont have sundaes.")
+else: print("we dont have sundaes.")
 
 if ("Chicken" in food):
     print("you can have chicken!")
-else print("We dont have chicken.") 
+else: print("We dont have chicken.") 
