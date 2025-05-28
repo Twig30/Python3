60bcd6e37aac1d5766873bd77ea07ea5ac8f89d7
Twig30 <immanuel.delgado@stu.vertexacademies.org>
2025-05-28 18:14:06 +0000

    finnished 3.2.1

--- 321_lists_Delgado.py
+++ 321_lists_Delgado.py
@@ -1,1 +1,15 @@
 print("hello world!")
+food = ["Burger", "Shake", "Fries" , "pizza", "chicken"]
+
+print(food[0], "is located at index 0.")
+print(food[1], "is located at index 1.")
+print(food[2], "is located at index 2.")
+print(food[3], "is located at index 3.")
+print(food[-1], "is located at index -1.")
+if("sundae" in food):
+    print("You can have a sundae!")
+else print("we dont have sundaes.")
+
+if ("Chicken" in food):
+    print("you can have chicken!")
+else print("We dont have chicken.") 
