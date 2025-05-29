3359a654d2ae9d2b8a59db367bfbdee17c31b5f7
Twig30 <immanuel.delgado@stu.vertexacademies.org>
2025-05-29 14:16:09 -0400

    Create 322_social_media_Delgado.py

---
+++ 322_social_media_Delgado.py
@@ -0,0 +1,9 @@
+from Post import Post
+
+all_posts_archive = []
+
+# your code here
+Username = input("Enter your username here!")
+print("welcome"+ " " + Username)
+User_input = input("What would you like to do?")
+if (User_input) == "new"
